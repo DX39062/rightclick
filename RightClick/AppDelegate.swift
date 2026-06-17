acc737c2ceaddb7462dd1ea7e5aff76affd95970
DX39062 <871513540@qq.com>
2026-06-18 01:22:56 +0800

    fix: quit helper app after window closes

--- RightClick/AppDelegate.swift
+++ RightClick/AppDelegate.swift
@@ -4,4 +4,8 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.activate(ignoringOtherApps: true)
     }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
+    }
 }
